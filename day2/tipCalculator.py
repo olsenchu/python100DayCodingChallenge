1c647d2d5bc70dc4ae36ebb02f2e28fd35a2beb0
olsenchu <jjavier7@hawkmail.hccfl.edu>
2022-03-06 15:01:36 -0500

    Update to day2

---
+++ day2/tipCalculator.py
@@ -0,0 +1,8 @@
+print("Welcome to the tip calculator!")
+bill_total = input("What was the total bill?\n")
+tip_percent = input("What % tip would you like to give?\n")
+tip_factor = ((int(tip_percent) / 100)) + 1
+guest_count = input("How many people to split the bill?\n")
+cost_per_guest = ((float(bill_total)) / (int(guest_count))) * (tip_factor)
+cost_per_guest = round(cost_per_guest, 2)
+print(f"Each person should pay ${cost_per_guest}.")
