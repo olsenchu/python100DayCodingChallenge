eb6fc770ed8b32e421f17dbfa3d15ae3afdc6208
olsenchu <jjavier7@hawkmail.hccfl.edu>
2022-03-06 14:25:05 -0500

    Update to day1

---
+++ day1/bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+home_town = input("What city did you grow up in?\n")
+pet_name = input("What's the name of your first pet?\n")
+band_name = print(f"Your band name could be {home_town} {pet_name}!")
